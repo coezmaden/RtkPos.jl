1162c147ae11734b021d0fb6c28ffd79af3fad19
Can Ozmaden <git@canozmaden.com>
2023-03-18 10:50:22 +0100

    Implement az, el, gps timestamp parsing

--- src/RtkPos.jl
+++ src/RtkPos.jl
@@ -12,6 +12,9 @@ using CategoricalArrays
 include("parse_body.jl")
 include("parse_stat.jl")
 
+GPSEPOCH = DateTime(1980, 1, 6, 0, 0, 0)
+GPSLEAP = 18
+
 function load_pos(f)
     df = DataFrame(
         Timestamp = ZonedDateTime[],
@@ -30,25 +33,24 @@ end
 
 function load_pos_stat(f)
     df = DataFrame(
-        Timestamp = ZonedDateTime[],
-        sys = [],
-        satnum = Int[],
+        Timestamp = DateTime[],
+        prn = String[],
         az = Float64[],
-        el = Float64[],
-        prange_res = Float64[],
-        cphase_res = Float64[],
-        valid = Bool[],
-        SNR = Float64[],
-        fix = Int64[],
-        slip = Int64[],
-        lock = Int64[],
-        outc = Int64[],
-        slipc = Int64[],
-        rejc = Int64[],
-        icbias = Float64[],
-        bias = Float64[],
-        bias_var = Float64[],
-        lambda = Float64[]
+        el = Float64[]
+        # prange_res = Float64[],
+        # cphase_res = Float64[],
+        # valid = Bool[],
+        # SNR = Float64[],
+        # fix = Int64[],
+        # slip = Int64[],
+        # lock = Int64[],
+        # outc = Int64[],
+        # slipc = Int64[],
+        # rejc = Int64[],
+        # icbias = Float64[],
+        # bias = Float64[],
+        # bias_var = Float64[],
+        # lambda = Float64[]
     )
     open(f) do s
         parse_stat!(df, s)
--- src/parse_stat.jl
+++ src/parse_stat.jl
@@ -3,6 +3,25 @@ function parse_stat!(df::DataFrame, s::IOStream)
     lines =  readlines(s)
     # For now only read the $SAT messages 
     # TODO implement more parsing options
-    slines = split.(lines[contains.(lines, "\$SAT")], ',')
+    sat_lines = split.(lines[contains.(lines, "\$SAT")], ',')
     
+    for line in sat_lines
+        # Timestamp
+        GPS_week = line[2]
+        TOW = parse(Float64, line[3])
+        seconds = floor(Int, TOW)
+        mseconds = floor(Int, 1e3*(TOW - seconds))
+        t = GPSEPOCH + Week(GPS_week) + Second(seconds) + Millisecond(mseconds) - Second(GPSLEAP)
+        push!(df.Timestamp, t)
+
+        # PRN
+        push!(df.prn, line[4])
+
+        # Azimuth
+        push!(df.az, parse(Float64, line[6]))
+
+        # Elevation
+        push!(df.el, parse(Float64, line[7]))
+    end
+
 end
